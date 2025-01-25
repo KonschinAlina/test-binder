93bfc5b1fe6a85b9f9d0e87a124cec14e6baf188
KonschinAlina <149383448+KonschinAlina@users.noreply.github.com>
2025-01-25 17:27:31 +0100

    Delete src/ros_utils/ros_utils directory

--- src/ros_utils/ros_utils/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="viz_marker_publisher",
-    version="0.1.0",
-    description="A ROS utility for publishing markers",
-    packages=find_packages(),
-    install_requires=[],
-)
