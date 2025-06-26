c540fa837bd98de2760b5218f44256ecdb0d0662
KonschinA <alina.konschin@gmail.com>
2025-06-26 13:47:47 +0200

    - update ba

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Launch the ROS core and web tools when containter starts
-source ${HOME}/workspace/ros/devel/setup.bash
+#source ${HOME}/workspace/ros/devel/setup.bash
+source /opt/ros/overlay_ws/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
@@ -12,13 +13,11 @@ MONGODB_URL=mongodb://127.0.0.1:27017
 mongod --fork --logpath ${HOME}/mongod.log
 
 # Create a symbolic link to the folder neem_data
-ln -s /neem_data ${PWD}/neem_data
+#ln -s /neem_data ${PWD}/neem_data
 
 # Launch Knowrob
 export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob knowrob.launch &
-
-cp ${HOME}/work/binder/webapps.json ${HOME}/workspace/ros/src/rvizweb/webapps/json.apps
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
---
+++ prolog/test_load_sem_map.pl
@@ -0,0 +1,6 @@
+:- module(test_load_sem_map,
+	[ test/1
+	]).
+
+test(A):-
+	A = 'Hello World!'.
