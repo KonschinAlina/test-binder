c3671a79595cf340e58c61bca0e395f330d9b9d1
KonschinA <alina.konschin@gmail.com>
2025-06-27 11:07:55 +0200

    - removed extra mongo install

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,23 +3,15 @@
 # Launch the ROS core and web tools when containter starts
 #source ${HOME}/workspace/ros/devel/setup.bash
 source /opt/ros/overlay_ws/devel/setup.bash
+
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/work/binder/rviz_configs/pr2_config.json &
-
-# Add other startup programs here
-
-# Start MongoDB and save data on working directory
-MONGODB_URL=mongodb://127.0.0.1:27017
-mongod --fork --logpath ${HOME}/mongod.log
-
-# Create a symbolic link to the folder neem_data
-#ln -s /neem_data ${PWD}/neem_data
 
 # Launch Knowrob
 export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob knowrob.launch &
 
-#cp ${HOME}/work/binder/webapps.json ${ROS_WS}/src/rvizweb/webapps/app.json
+#cp ${HOME}/test-binder/binder/webapps.json opt/ros/overlay_ws/src/rvizweb/webapps/app.json
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
