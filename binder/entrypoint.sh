89c62d209083789be770727e59b8481c2f09399e
KonschinA <alina.konschin@gmail.com>
2025-07-09 12:08:42 +0200

    - changes in dockerfile, changed list to requirements.txt - deleted not needed files - changes in entrypoint.sh - added a setup.rviz - saved a workspace - added in test_map_loading a line to start rviz in vnc

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+set -e 
 
-# Launch the ROS core and web tools when containter starts
-#source ${HOME}/workspace/ros/devel/setup.bash
+echo "Starting entrypoint..."
 source /opt/ros/overlay_ws/devel/setup.bash
-
+source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/work/binder/rviz_configs/pr2_config.json &
+sleep 5 
 
-# Launch Knowrob
-export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
+#export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob_ros knowrob.launch &
 
-#cp ${HOME}/test-binder/binder/webapps.json opt/ros/overlay_ws/src/rvizweb/webapps/app.json
+jupyter lab workspaces import ${HOME}/work/binder/BA_project.jupyterlab-workspace
 
-# The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
