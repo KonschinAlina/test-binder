66d22889b00e7a811605ae3c406031d0c3ec4dd7
KonschinA <alina.konschin@gmail.com>
2025-07-11 11:49:35 +0200

    - saved a workspace - added local rviz start

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,14 +2,15 @@
 set -e 
 
 echo "Starting entrypoint..."
+
+source /opt/ros/noetic/setup.bash
 source /opt/ros/overlay_ws/devel/setup.bash
-source ${HOME}/workspace/ros/devel/setup.bash
+
 roscore &
-roslaunch --wait rvizweb rvizweb.launch config_file:=${HOME}/work/binder/rviz_configs/pr2_config.json &
-sleep 5 
 
 #export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob_ros knowrob.launch &
+sleep 5
 
 jupyter lab workspaces import ${HOME}/work/binder/BA_project.jupyterlab-workspace
 
