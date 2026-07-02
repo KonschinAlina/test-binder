a0ac920214379017cee9e4d31f4487dc9d148040
KonschinAlina <149383448+KonschinAlina@users.noreply.github.com>
2026-07-02 16:58:20 +0200

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-set -e 
+set -ex
 
 echo "Starting entrypoint..."
 
 source /opt/ros/noetic/setup.bash
 source /opt/ros/overlay_ws/devel/setup.bash
 
+echo "Starting roscore..."
 roscore &
 sleep 5
+
+echo "Jupyter version:"
+jupyter --version
 
 #roslaunch --wait knowrob_ros knowrob.launch &
 #sleep 5
 
-jupyter lab workspaces import ${HOME}/work/binder/BA_workspace.jupyterlab-workspace
+echo "Starting Jupyter..."
+#jupyter lab workspaces import ${HOME}/work/binder/BA_workspace.jupyterlab-workspace
 
 #jupyter lab --ip=0.0.0.0 --no-browser --allow-root
 #jupyter lab  --ip=0.0.0.0 --no-browser --NotebookApp.notebook_dir='/home/jovyan/work/binder/BA_workspace.jupyterlab-workspace'
 #jupyter lab workspaces import ${HOME}/work/binder/BA_workspace.jupyterlab-workspace
 #echo "Launching Jupyter in $(pwd)..."
 
-exec "$@"
+exec jupyter lab \
+    --debug \
+    --ip=0.0.0.0 \
+    --no-browser \
+    --ServerApp.token='' \
+    --ServerApp.password=''
+#"$@"
